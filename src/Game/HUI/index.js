8fe2bbcc941b8b37f3928ee22c72f736ed54c63a
Matthew-Burfield <m1.burfield@gmail.com>
2017-03-30 18:58:59 +1000

    Added HP and EXP bars to HUD

--- src/Game/HUI/index.js
+++ src/Game/HUI/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const HUI = ({ player }) => {
-  const pStyle = {
-    margin: '2px',
-  };
+  const playerLevel = `Level ${player.level}`;
 
   return (
-    <div style={{ fontSize: '0.8em', textAlign: 'left', marginLeft: '20px' }}>
+    <div style={{ margin: 'auto', fontSize: '0.8em' }}>
       <h2>Player</h2>
-      <p style={pStyle}>Health: <b>{player.currHealth}</b></p>
-      <p style={pStyle}>Level: <b>{player.level}</b></p>
-      <p style={pStyle}>Exp: <b>{player.exp}</b></p>
-      <p style={pStyle}>Weapon: <b>{player.weapon.name}</b></p>
-      <p style={pStyle}>Shield: <b>{player.shield}</b></p>
+      <PlayerBar
+        color="red"
+        darkColor="darkRed"
+        height={20}
+        maxVal={player.maxHealth}
+        currVal={player.currHealth}
+      />
+      <PlayerBar
+        color="goldenrod"
+        darkColor="#322A00"
+        height={10}
+        maxVal={player.nextLevelExp}
+        currVal={player.currExp}
+      />
+      <p>Weapon: <b>{player.weapon.name}</b></p>
+      <p>Shield: <b>{player.shield}</b></p>
     </div>
   );
 };
@@ -20,8 +29,10 @@ const HUI = ({ player }) => {
 HUI.propTypes = {
   player: React.PropTypes.shape({
     currHealth: React.PropTypes.number.isRequired,
+    maxHealth: React.PropTypes.number.isRequired,
     level: React.PropTypes.number.isRequired,
-    exp: React.PropTypes.number.isRequired,
+    currExp: React.PropTypes.number.isRequired,
+    nextLevelExp: React.PropTypes.number.isRequired,
     weapon: React.PropTypes.shape({
       type: React.PropTypes.string.isRequired,
       name: React.PropTypes.string.isRequired,
@@ -29,6 +40,27 @@ HUI.propTypes = {
     }).isRequired,
     shield: React.PropTypes.string.isRequired,
   }).isRequired,
+};
+
+
+const PlayerBar = ({ color, darkColor, height, maxVal, currVal }) => {
+  const widthMaxVal = window.innerWidth * 0.6;
+  const widthCurrVal = (currVal / maxVal) * widthMaxVal;
+
+  return (
+    <div className="playerHealthBar" style={{ margin: 'auto', width: widthMaxVal, height, backgroundColor: darkColor, borderRadius: height / 2 }}>
+      <div style={{ margin: 'left', width: widthCurrVal, height, backgroundColor: color, borderRadius: height / 2 }}>{name}</div>
+    </div>
+  );
+};
+
+
+PlayerBar.propTypes = {
+  color: React.PropTypes.string.isRequired,
+  darkColor: React.PropTypes.string.isRequired,
+  height: React.PropTypes.number.isRequired,
+  maxVal: React.PropTypes.number.isRequired,
+  currVal: React.PropTypes.number.isRequired,
 };
 
 export default HUI;
